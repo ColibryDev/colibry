f2dc9175a6d081ee88e2dd24ccd8aa14cdb43023
martwerlen <martin.werlen@gmail.com>
2015-08-11 23:26:58 -0400

    Profile done

--- Profiles.js
+++ Profiles.js
@@ -1,40 +1,82 @@
-Schemas = {};
+Schema = {};
 
 
 PROFILES = new Mongo.Collection('Profiles');
-PROFILES.attachSchema(new SimpleSchema({
-  firstname: {
+
+Schema.UserProfile = new SimpleSchema({
+   firstname: {
     type: String,
-    label: "First name",
-    max: 50
+    label: 'First Name',
+    regEx: /^[a-zA-Z-]{2,25}$/,
+    optional: true,
+    autoform: {
+      'label-type': 'floating',
+      placeholder: 'First Name'
+    }
   },
-  name: {
+  lastname: {
     type: String,
-    label: "Name",
-    max: 50
-  },
-  mail: {
-    type: String,
-    label: "E-mail",
-    max: 50
+    label: 'Last Name',
+    regEx: /^[a-zA-Z-]{2,25}$/,
+    optional: true,
+    autoform: {
+      'label-type': 'floating',
+      placeholder: 'Last Name'
+    }
   },
   birthday: {
     type: Date,
     label: "Birthday",
     optional: true
   }
-}));
+});
+
+Schema.User = new SimpleSchema({
+
+    emails: {
+        type: [Object],
+        // this must be optional if you also use other login services like facebook,
+        // but if you use only accounts-password, then it can be required
+        optional: true
+    },
+    "emails.$.address": {
+        type: String,
+        regEx: SimpleSchema.RegEx.Email
+    },
+    "emails.$.verified": {
+        type: Boolean
+    },
+    createdAt: {
+        type: Date
+
+    },
+    profile: {
+        type: Schema.UserProfile,
+        optional: true
+    },
+    services: {
+        type: Object,
+        optional: true,
+        blackbox: true
+    }
+
+  }); 
+
+
+Meteor.users.attachSchema(Schema.User);
 
 
 
-PROFILES.allow({
-    insert: function(_id, doc){
-    return doc; //&& doc._id === _id;
+
+Meteor.users.allow({
+  insert: function(userId, doc){
+    return doc && doc.userId === userId;
   },
-  update: function(_id, doc){
-    return doc; // && doc._id === _id;
+  update: function(userId, doc){
+    return doc && doc.userId === userId;
   }
 })
+
 /*
 var ProfileHooks = {
   before: {
@@ -55,6 +97,10 @@ AutoForm.addHooks(Meteor.userId(), ProfileHooks);
 if (Meteor.isClient) {
 
   Template.profile.helpers({
+
+  user: function(){
+    return Meteor.userId;
+  },
 
   'CurrentUser': function(){
   var currentUserId = Meteor.userId();
