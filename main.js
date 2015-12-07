3d831cdda92444f3a3c4a27d14427f34d3a04720
martwerlen <martin.werlen@gmail.com>
2015-12-07 09:16:03 +0100

    fixed nav bar Welcome page router 404 error si pas connu… :)

--- main.js
+++ main.js
@@ -3,14 +3,21 @@ if (Meteor.isClient) {
 Template.main.rendered = function(){
 
     // Add special class for handel top navigation layout
-    $('body').addClass('top-navigation navbar-fixed-top navbar-scroll');
-
+    $('body').addClass('top-navigation');
+    $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
+    $("body").removeClass('boxed-layout');
+            $("body").addClass('fixed-nav');
+    
 }
 
 Template.main.destroyed = function(){
 
     // Remove special top navigation class
-    $('body').removeClass('top-navigation navbar-fixed-top navbar-scroll');
+    $('body').removeClass('top-navigation');
+   $(".navbar-fixed-top").removeClass('navbar-fixed-top').addClass('navbar-static-top');
+            $("body").removeClass('fixed-nav');
+    $('body').removeClass('navbar-static-top');
+
 };
 
 // Puisque Meteor ne peut pas attribuer de class au tag body, il fayt le faire côté JS
@@ -20,7 +27,7 @@ Meteor.startup(function() {
 
 
 //Fonction lorsqu'on clique sur logout, cela logout l'utilisateur, le renvoit vers la page login et lui affiche un message à travers DHTMLX pour lui dire qu'il est à présent déconnecté
-Template.navBar.events({
+Template.topNavBar.events({
     'click .logout': function(event){
         event.preventDefault();
         dhtmlx.message({ type:"error", text:"You logged out", expire: 1500}); 
@@ -31,7 +38,7 @@ Template.navBar.events({
     }
 });
 
-Template.navBar.helpers({
+Template.topNavBar.helpers({
 	getProfilePic: function () {
 	var currentUser = Meteor.user();
 	var profilePicId = currentUser.profile.pic;
